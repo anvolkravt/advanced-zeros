377f583753512da60833f13a45952294f730de1d
anvolkravt <anvolkravt@ya.ru>
2019-03-21 11:20:27 +0300

    feat: implement function that counts trailing zeros in a factorial

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  function getAllFactorsFor(base) {
+    var factors = [];
+    for (i = 2; i <= base; i++) {
+      while ((base % i) === 0) {
+        factors.push(i);
+        base /= i;
+      }
+    }
+  return factors;
+}
+
+  let baseFactors = getAllFactorsFor(base);
+  let maxFactor = Math.max(...baseFactors);
+
+  for (sum = 0, i = 1;  ; i++) {
+    if (Math.floor( number / Math.pow(maxFactor, i) ) == 0) break;
+    sum += Math.floor( number / Math.pow(maxFactor, i) );
+  }
+
+  for (i = 0, counter = 0; i < baseFactors.length; i++) {
+    if (baseFactors[i] == maxFactor) counter++;
+  }
+
+  return Math.floor(sum / counter);  
 }
